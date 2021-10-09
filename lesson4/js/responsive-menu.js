09938a01dbc3b1010c2c7060d96031c0f61eeb85
Raquel Eyler <raquel.e.badal@gmail.com>
2021-10-09 00:24:27 -0600

    Hopefully this works. Had to reinit git on local machine.

---
+++ lesson4/js/responsive-menu.js
@@ -0,0 +1,11 @@
+let hambutton = document.querySelector('.hamburger');
+let mainnav = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+//Print out date
+let d = new Date()
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+document.getElementById("date").textContent = d.toLocaleDateString('en-UK',options);

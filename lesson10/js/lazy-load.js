c27da01ba495578106c57c19c6915368cd01511b
Rockies93 <raquel.e.badal@gmail.com>
2021-11-20 23:36:10 -0700

    Lesson 10 code

---
+++ lesson10/js/lazy-load.js
@@ -0,0 +1,37 @@
+//Calls datasrc 
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+//switch src and datasrc and remove datasrc
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+//load new src
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+
+//lazy load
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} 
+else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
+
+  
